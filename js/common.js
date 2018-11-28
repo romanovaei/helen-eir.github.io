d172758640b50e42a1ac95ddc4dda11b5f6d3222
romanovaei <romanovaeiw@gmail.com>
2018-11-28 11:41:17 +0400

    переписала кнопку меню на jquery

--- js/common.js
+++ js/common.js
@@ -1,19 +1,13 @@
-function Menu(accordeon) {
-  var elem = accordeon.elem;
+$(function () {
+  var one = function() {
 
-  elem.mousedown = function() {
-    return false;
-  }
+	  $(this).toggleClass("nav__button_menu_close");
+    $(this).toggleClass("nav__button_menu_open");
+	  $("header").toggleClass("header__page_close");
+    $("header").toggleClass("header__page_open");
+    $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__item_hidden");
+    $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__menu_open");
+	};
 
-  elem.onclick = function(event) {
-    if (event.target.closest(".nav__button_menu")) {
-      elem.classList.toggle("nav__menu_open");
-    }
-  };
-
-}
-
-// использование
-var menu = new Menu({
-  elem: document.getElementById("menu_open")
+	$(".nav__button_menu").click(one);
 });
