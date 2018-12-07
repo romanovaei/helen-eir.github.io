92ac5f8a9756179e91654be4bcb4f2db08640a0e
helen-eir <44238864+helen-eir@users.noreply.github.com>
2018-12-07 11:35:25 +0400

    главная страница оформлена

--- js/common.js
+++ js/common.js
@@ -1,13 +1,22 @@
 $(function () {
-  var one = function() {
+  var openMenu = function() {
 
 	  $(this).toggleClass("nav__button_menu_close");
     $(this).toggleClass("nav__button_menu_open");
-	  $("header").toggleClass("header__page_close");
-    $("header").toggleClass("header__page_open");
     $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__item_hidden");
     $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__menu_open");
 	};
 
-	$(".nav__button_menu").click(one);
+	$(".nav__button_menu").click(openMenu);
+
+  (function() {
+      $(".modal__button_open").bind('click', function() {
+          modal.showModal();
+      });
+
+      $(".modal__button_close").bind('click', function() {
+          modal.close();
+      });
+  })();
+
 });
