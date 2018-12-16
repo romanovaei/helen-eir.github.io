fdcbb2f4b9f9b89bfc79be78a7b04fa0eec0354c
helen-eir <44238864+helen-eir@users.noreply.github.com>
2018-12-16 06:34:55 +0400

    Правки по замечаниям

--- js/common.js
+++ js/common.js
@@ -5,16 +5,19 @@ $(function () {
     $(this).toggleClass("nav__button_menu_open");
     $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__item_hidden");
     $(".nav__item_catalog, .nav__item_order, .nav__item_search, .nav__item_cart").toggleClass("nav__menu_open");
+
 	};
 
 	$(".nav__button_menu").click(openMenu);
 
+
+
   (function() {
-      $(".modal__button_open").bind('click', function() {
+      $(".modal__button_open").bind("click", function() {
           modal.showModal();
       });
 
-      $(".modal__button_close").bind('click', function() {
+      $(".modal__button_close").bind("click", function() {
           modal.close();
       });
   })();
